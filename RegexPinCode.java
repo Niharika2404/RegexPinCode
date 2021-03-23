82a63944a97439b6b31864bae24fb253f9207d72
Niharika2404 <niharikang24@gmail.com>
2021-03-23 22:02:33 +0530

    Welcome to the Regex

---
+++ RegexPinCode.java
@@ -0,0 +1,20 @@
+package RegexPinCode;
+
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+public class RegexPinCode {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter the PinCode: ");
+		String pincode = sc.nextLine();
+		
+		boolean result = new RegexPinCode().validatePinCode(pincode);
+		System.out.println(result);
+	}
+	
+	public boolean validatePinCode(String pincode) {
+		return Pattern.compile("[0-9]{6}").matcher(pincode).matches();
+	}
+}
